f50af35bfe8787a42e6cb7f2f8bc66e3554d60fc
EmeraldWolfram <stevenfgy@yahoo.com>
2015-07-20 12:22:32 +0800

    added MailBox.h

---
+++ app/MailBox.h
@@ -0,0 +1,25 @@
+#ifndef MailBox_H
+#define MailBox_H
+
+#include "Mutex.h"
+#include "Semaphore.h"
+
+typedef struct{
+	int *buufer;
+	int size;
+	Semaphore sloatAvailbale;
+	Semaphore slotsUsed;
+	int writeIndex;
+	int readiIndex;
+	Mutex mutexForBuffer;
+}MailBox;
+
+void mailBoxInit( MailBox *mainlBox, int size);
+void mailBoxSendMessage(MailBox *milBox, int message);
+int mailBoxGetMessage(MailBox *mailBox);
+
+
+
+
+
+#endif // MailBox_H
