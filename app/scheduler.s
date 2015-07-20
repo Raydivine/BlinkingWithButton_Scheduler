bf3161786816b58151248b8d61c20f9cbd6aea71
EmeraldWolfram <stevenfgy@yahoo.com>
2015-07-20 11:29:38 +0800

    change ldr r4,r0 to mov r4,r0

--- app/scheduler.s
+++ app/scheduler.s
@@ -38,9 +38,9 @@ waitForTaskSwitch:
 
 SysTick_Handler:
 //First
-  push 	{r4-r11}          //1. push all necessary register
-  ldr 	r0, =mainTcb      //2. load mainTcb
-  movs	r1, #0            //3. save SP into mainTcb.sp 
+  	push 	{r4-r11}          //1. push all necessary register
+  	ldr 	r0, =mainTcb      //2. load mainTcb
+ 	movs	r1, #0            //3. save SP into mainTcb.sp
 	add		r1, sp            //
 	str 	r1, [r0, #TCB_SP] //   (r0 is mainTcb, r1 is sp, and r1 is store into r0.sp)
 	ldr 	r0, =taskOneTcb   //4. Load taskOneTcb
@@ -48,24 +48,24 @@ SysTick_Handler:
 	pop 	{r4-r11}          //6. pop all pushed register
 	pop 	{r0-r3}           //
 	pop 	{r12}             //
-  bx     lr               //7. return from interrupt
+ 	 bx     lr               //7. return from interrupt
   
 //Second
-  push  {r4-r11}          //1. push all necessary register
-  ldr   r4, =runningTcb   //2. a)load runningTcb into r4   
+  	push  {r4-r11}          //1. push all necessary register
+  	ldr   r4, =runningTcb   //2. a)load runningTcb into r4
   //ldr   r4, [r4]          //   b)load r4 into r4
-  str   sp, [r4, #TCB_SP] //3. store SP into runningTcb.sp (r4 is runningTcb)
-  push  {r7, lr}          //4. a) push r7 and lr
-  ldr   r0, =readyQueue   //   b) load readyQueue into r0(readyQueue is LinkedList)
-  bl    removeHead        //   c) removeHead(r0)
-  mov   r5, r0            //   d) r5 = r0  ( r5 is the list )
-  ldr   sp, [r5, #TCB_SP] //5. load R5.sp into sp 
-  ldr   r5, =runningTcb   //6. load runningTcb into r5  
-  pop   {r7, lr}          //7. a) pop r7 and lr
-  ldr   r0, r4            //   b) load r4 to r0
-  bl    addDataToTail     //   c) add r1 to tail ( r1 also equal r4)
-  pop   {r4-r11}          // pop all pushed register
-  bx    lr                // return from interrupt 
+  	str   sp, [r4, #TCB_SP] //3. store SP into runningTcb.sp (r4 is runningTcb)
+  	push  {r7, lr}          //4. a) push r7 and lr
+ 	ldr   r0, =readyQueue   //   b) load readyQueue into r0(readyQueue is LinkedList)
+ 	bl    removeHead        //   c) removeHead(r0)
+ 	mov   r5, r0            //   d) r5 = r0  ( r5 is the list )
+  	ldr   sp, [r5, #TCB_SP] //5. load R5.sp into sp
+  	ldr   r5, =runningTcb   //6. load runningTcb into r5
+  	pop   {r7, lr}          //7. a) pop r7 and lr
+  	mov   r0, r4            //   b) load r4 to r0
+  	bl    addDataToTail     //   c) add r1 to tail ( r1 also equal r4)
+  	pop   {r4-r11}          // pop all pushed register
+  	bx    lr                // return from interrupt
   
 // extra space
 //     XPSR
